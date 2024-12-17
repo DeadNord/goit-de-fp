72044ce26563e38aa9cfefb79d61a2205a970391
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-12-17 19:58:20 +0100

    refactor of dag

--- dags/project_solution.py
+++ dags/project_solution.py
@@ -17,46 +17,53 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    # Общие аргументы Spark
-    spark_conf = [
-        "--master",
-        "spark://217.61.58.159:7077",
-        "--deploy-mode",
-        "client",
-        "--conf",
-        "spark.executor.memory=2g",
-        "--conf",
-        "spark.driver.memory=2g",
-        "--conf",
-        "spark.executor.cores=2",
-    ]
+    # Spark Master для Standalone режима
+    spark_master = "spark://217.61.58.159:7077"
 
-    # Етап 1: landing_to_bronze
+    # Задача: landing_to_bronze
     landing_to_bronze = SparkSubmitOperator(
         task_id="landing_to_bronze",
-        application="dags/landing_to_bronze.py",
+        application="/opt/airflow/dags/landing_to_bronze.py",
+        conn_id=None,
         name="landing_to_bronze_job",
-        application_args=spark_conf,
         verbose=True,
+        conf={
+            "spark.master": spark_master,  # Указываем master здесь
+            "spark.executor.memory": "2g",
+            "spark.driver.memory": "2g",
+            "spark.executor.cores": "2",
+        },
     )
 
-    # Етап 2: bronze_to_silver
+    # Задача: bronze_to_silver
     bronze_to_silver = SparkSubmitOperator(
         task_id="bronze_to_silver",
-        application="dags/bronze_to_silver.py",
+        application="/opt/airflow/dags/bronze_to_silver.py",
+        conn_id=None,
         name="bronze_to_silver_job",
-        application_args=spark_conf,
         verbose=True,
+        conf={
+            "spark.master": spark_master,
+            "spark.executor.memory": "2g",
+            "spark.driver.memory": "2g",
+            "spark.executor.cores": "2",
+        },
     )
 
-    # Етап 3: silver_to_gold
+    # Задача: silver_to_gold
     silver_to_gold = SparkSubmitOperator(
         task_id="silver_to_gold",
-        application="dags/silver_to_gold.py",
+        application="/opt/airflow/dags/silver_to_gold.py",
+        conn_id=None,
         name="silver_to_gold_job",
-        application_args=spark_conf,
         verbose=True,
+        conf={
+            "spark.master": spark_master,
+            "spark.executor.memory": "2g",
+            "spark.driver.memory": "2g",
+            "spark.executor.cores": "2",
+        },
     )
 
-    # Задачи выполняются последовательно
+    # Последовательность задач
     landing_to_bronze >> bronze_to_silver >> silver_to_gold
