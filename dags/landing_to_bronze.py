d5ef69f8cc91e2ca34820bf398c23ac480f75efe
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-12-17 19:09:26 +0100

    moved connet into dag

--- dags/landing_to_bronze.py
+++ dags/landing_to_bronze.py
@@ -3,15 +3,19 @@
 import requests
 from pyspark.sql import SparkSession
 
+
 def download_data(local_file_path):
     url = "https://ftp.goit.study/neoversity/"
     downloading_url = url + local_file_path + ".txt"
     response = requests.get(downloading_url)
     if response.status_code == 200:
-        with open(local_file_path + ".txt", 'wb') as file:
+        with open(local_file_path + ".txt", "wb") as file:
             file.write(response.content)
     else:
-        raise Exception(f"Failed to download the file. Status code: {response.status_code}")
+        raise Exception(
+            f"Failed to download the file. Status code: {response.status_code}"
+        )
+
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("LandingToBronze").getOrCreate()
@@ -21,7 +25,11 @@ if __name__ == "__main__":
     for table in tables:
         download_data(table)
         # Читаємо CSV
-        df = spark.read.option("header", "true").option("inferSchema", "true").csv(table + ".txt")
+        df = (
+            spark.read.option("header", "true")
+            .option("inferSchema", "true")
+            .csv(table + ".txt")
+        )
         # Записуємо в bronze
         df.write.mode("overwrite").parquet(f"bronze/{table}")
 
--- dags/project_solution.py
+++ dags/project_solution.py
@@ -2,40 +2,61 @@ from datetime import datetime
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
+# Аргументы по умолчанию
 default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
-    'retries': 1
+    "owner": "eod_airflow",
+    "start_date": datetime(2023, 1, 1),
+    "retries": 1,
 }
 
-with DAG('batch_pipeline_dag',
-         default_args=default_args,
-         schedule_interval='@daily',
-         catchup=False) as dag:
+# DAG
+with DAG(
+    "eod_batch_pipeline_dag",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
 
-    # Запуск landing_to_bronze (Етап 1 Ч.2)
+    # Общие аргументы Spark
+    spark_conf = [
+        "--master",
+        "spark://217.61.58.159:7077",
+        "--deploy-mode",
+        "client",
+        "--conf",
+        "spark.executor.memory=2g",
+        "--conf",
+        "spark.driver.memory=2g",
+        "--conf",
+        "spark.executor.cores=2",
+    ]
+
+    # Етап 1: landing_to_bronze
     landing_to_bronze = SparkSubmitOperator(
-        task_id='landing_to_bronze',
-        application='dags/landing_to_bronze.py',
-        conn_id='spark_default',
-        verbose=1
+        task_id="landing_to_bronze",
+        application="dags/landing_to_bronze.py",
+        name="landing_to_bronze_job",
+        application_args=spark_conf,
+        verbose=True,
     )
 
-    # Запуск bronze_to_silver (Етап 2 Ч.2)
+    # Етап 2: bronze_to_silver
     bronze_to_silver = SparkSubmitOperator(
-        task_id='bronze_to_silver',
-        application='dags/bronze_to_silver.py',
-        conn_id='spark_default',
-        verbose=1
+        task_id="bronze_to_silver",
+        application="dags/bronze_to_silver.py",
+        name="bronze_to_silver_job",
+        application_args=spark_conf,
+        verbose=True,
     )
 
-    # Запуск silver_to_gold (Етап 3 Ч.2)
+    # Етап 3: silver_to_gold
     silver_to_gold = SparkSubmitOperator(
-        task_id='silver_to_gold',
-        application='dags/silver_to_gold.py',
-        conn_id='spark_default',
-        verbose=1
+        task_id="silver_to_gold",
+        application="dags/silver_to_gold.py",
+        name="silver_to_gold_job",
+        application_args=spark_conf,
+        verbose=True,
     )
 
-    # Послідовність виконання: landing_to_bronze -> bronze_to_silver -> silver_to_gold
+    # Задачи выполняются последовательно
     landing_to_bronze >> bronze_to_silver >> silver_to_gold
